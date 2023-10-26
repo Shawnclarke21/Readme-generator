8c49eaa9de34e365bb312dbc0caacebd85143651
shawn <srclarke777@gmail.com>
2023-10-25 21:21:50 -0700

    added code to get the prompts to work and write in questions and repsonses as well as giving choices for licensing

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,89 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require ('fs');
+const path =require('path')
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ {
+    type:"input",
+    message: "What is the project name?",
+    name:"title",
+    validate:(value)=>{if(value){return true}else {return'input needed'}}
+},
+
+{
+    type:"input",
+    message: "project description?",
+    name:"description",
+    validate:(value)=>{if(value){return true}else {return'input needed'}}
+},
+
+{
+    type:"input",
+    message: "table of contents?",
+    name:"content",
+    validate:(value)=>{if(value){return true}else {return'input needed'}}
+},
+
+{
+    type:"input",
+    message: "how to install and run the project?",
+    name:"install",
+    validate:(value)=>{if(value){return true}else {return'input needed'}}
+},
+
+{
+    type:"input",
+    message: "how is the project used",
+    name:"usage",
+    validate:(value)=>{if(value){return true}else {return'input needed'}}
+},
+
+{
+    type:"input",
+    message: "credits?",
+    name:"credits",
+    validate:(value)=>{if(value){return true}else {return'input needed'}}
+},
+{
+    type:"checkbox",
+    message: "What License are you using?",
+    name:"License",
+    choices:['AFL-3.0','Apache-2.0','MPL-2.0','Artistic-2.0','MIT', 'none'],
+    validate:(value)=>{if(value){return true}else {return'input needed'}}
+},
+{
+    type:"input",
+    message: "What is your github username?",
+    name:"github",
+    validate:(value)=>{if(value){return true}else {return'input needed'}}
+},
+{
+    type:"input",
+    message: "What tests were ran?",
+    name:"tests",
+    validate:(value)=>{if(value){return true}else {return'input needed'}}
+},
+{
+    type:"input",
+    message: "What is your email?",
+    name:"email",
+    validate:(value)=>{if(value){return true}else {return'input needed'}}
+}
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+return fs.writeFileSync(path.join(process.cwd(), fileName),data)}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((responses)=>{
+        console.log('creating readme');
+        writeToFile('./Readme-generator/README.md',generateMarkdown({...responses}))
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,49 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license!=="none"){
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
+function renderLicenseLink(license) {
+  if (license!=="none"){
+    return '';
+}
+}
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license!=="none"){
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  https://github.com/${data.github}/${data/title}
+  # Description
+  ${data.description}
+  # Table of contents
+  *[installation](#install)
+  *[Usage](#usage)
+  *[Credits](#credits)
+  *[License](#license)
+  *[tests](#tests)
+  # installation
+  these following programs are necessary to install this program
+  # License
+  This project is licensed under ${data.license}
+  # credits
+  credits go to ${data.credits}
+  # tests
+  the following is needed to run tests ${data.tests}
+
+  if you have questions or suggests reach out to ${data.email}
+
 
 `;
 }
